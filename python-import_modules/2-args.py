c3c769ec5f9d3cb5bb5ac4722c9dfc6b54cabd19
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-01-25 20:58:01 +0000

    add task

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+    
+if __name__ == '__main__':
+    import sys
+    num_args = len(sys.argv) - 1
+    plural = 's' if num_args != 1 else ''
+
+    if (num_args == 0):
+        print("0 arguments.")
+    else:
+        print(f"{num_args} argument{plural}:")
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{i}: {arg}")
