be2aae20c074d14eb69630cf8f15bed27234dd21
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-01-28 06:26:14 +0000

    add task

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
+
     if not sentence:
         return (0, None)
     length = len(sentence)
     character = sentence[0]
-    return(length, character)
+
+    return (length, character)
