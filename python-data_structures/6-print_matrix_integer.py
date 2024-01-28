1559c7c8fda2e892cdb07b36bb4259f9469e3fa1
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-01-28 05:57:33 +0000

    add task

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, number in enumerate(row):
+            if i < len(row) - 1:
+                print("{:d}".format(number), end="")
+            else:
+                print("{:d}".format(number))
