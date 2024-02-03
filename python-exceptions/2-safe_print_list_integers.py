c6ab4513e4384232f21fb8a17c08800009272bd0
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-03 05:28:17 +0000

    add task

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
-            if type(my_list[i]) is int:
-                print("{:d}".format(my_list[i]), end='')
-                count += 1
-    except IndexError:
-        pass
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
+        except (IndexError, ValueError):
+            pass
     print()
     return count
