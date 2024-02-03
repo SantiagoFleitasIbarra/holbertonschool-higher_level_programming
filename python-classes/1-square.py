61451697a108b9dbef1cce72f67b511c86df67b1
SantiagoFleitasIbarra <6769holbertonstudents.com>
2024-02-03 19:55:19 +0000

    add task

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module define a square"""
+
+
+class Square:
+    """
+    this class define a square
+    private instance attribute: size
+    """
+    def __init___(self, size):
+        self.__size = size
+    pass
