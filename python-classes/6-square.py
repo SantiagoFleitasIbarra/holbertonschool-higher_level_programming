6f4ddddb62f1fb70bcb3df7b6976bc6ca508bc54
SantiagoFleitasIbarra <6769holbertonstudents.com>
2024-02-03 20:48:35 +0000

    add task

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -28,7 +28,7 @@ class Square:
     @position.setter
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2 or \
-            not all(isinstance(i, int) and i >= 0 for i in value):
+                not all(isinstance(i, int) and i >= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self._position = value
