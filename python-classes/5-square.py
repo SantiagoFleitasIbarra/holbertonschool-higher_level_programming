cc5feb52bc60f6bbef344d67337cb1fbc43b32ef
SantiagoFleitasIbarra <6769holbertonstudents.com>
2024-02-03 20:26:35 +0000

    add task

---
+++ python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module defines a square"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
+    
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
