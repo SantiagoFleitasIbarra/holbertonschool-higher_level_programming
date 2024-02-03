3786c7f73d24ef02fd217eb0196c92315b9958a9
SantiagoFleitasIbarra <6769holbertonstudents.com>
2024-02-03 20:57:32 +0000

    add comments

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -9,10 +9,12 @@ class Square:
     
     @property
     def size(self):
+        """to retrieve it"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """to set it"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -21,11 +23,14 @@ class Square:
             self.__size = value
     
     def area(self):
+        """returns the area of the square"""
         return self.__size * self.__size
 
     def my_print(self):
+        """prints the square"""
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__size):
+            for _ in range(self.__size):
                 print("#" * self.__size)
+    pass
