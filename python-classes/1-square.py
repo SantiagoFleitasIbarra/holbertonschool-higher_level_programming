1412c147a849b98d62e55706171f3520e00443a1
SantiagoFleitasIbarra <6769holbertonstudents.com>
2024-02-03 19:59:49 +0000

    fixed bug

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""This module define a square"""
+"""This module defines a square"""
 
 
 class Square:
-    """
-    this class define a square
-    private instance attribute: size
-    """
-    def __init___(self, size):
+    """This class defines a square
+    private instance attribute: size"""
+    def __init__(self, size):
         self.__size = size
     pass
