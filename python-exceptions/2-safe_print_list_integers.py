78a9c832850589a65d0cbcb1bcdae1545abb1b2a
SantiagoFleitasIbarra <6769holbertonstudents.com>
2024-02-03 19:31:00 +0000

    fixed bug

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end='')
+            print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (IndexError, ValueError):
+        except (TypeError, ValueError):
             pass
     print()
     return count
