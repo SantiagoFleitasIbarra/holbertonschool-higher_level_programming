7256c6b3a9b26842e5dc6755b74fd0f3683d7c3c
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-04 15:54:03 +0000

    add task, create readme and create tests

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this module defines the sum of a and b"""
+
+
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    a = int(a)
+    b = int(b)
+    return a + b
