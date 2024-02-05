15c5a5d71b9d28fce5cd7a4d41eba8132a38c535
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-05 00:29:26 +0000

    fixed bug

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,9 +4,11 @@
 
 def add_integer(a, b=98):
     """this define a sum of a and b"""
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
+
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    else:
-        return int(a) + int(b)
+
+    return int(a) + int(b)
