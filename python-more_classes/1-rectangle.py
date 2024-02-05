7822da4702a6f36809020dfde3a1b36e0db08279
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-05 15:46:02 +0000

    add task

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""this module defines a rectangle class"""
+
+
+class Rectangle:
+    """this defines the class"""
+
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(self.__height, int):
+            raise TypeError("height must be an integer")
+        elif self.__height == 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(self.__width, int):
+            raise TypeError("width must be an integer")
+        elif self.__width == 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    pass
