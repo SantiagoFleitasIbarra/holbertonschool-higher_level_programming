adb3ef1ad3915a1dbd1460dc5215f630a492e8f0
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-07 21:27:04 +0000

    add task

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+this module returns True if the object
+is an instance of a class that inherited
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """this defines it"""
+    return isinstance(obj, a_class)
