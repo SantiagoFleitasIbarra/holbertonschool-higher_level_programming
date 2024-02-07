6ce7671b869bd48f62344b218550208f24778fb0
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-07 21:36:35 +0000

    add task

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-this module returns True if the object
-is an instance of a class that inherited
-"""
+"""this module returns True or False"""
 
 
 def inherits_from(obj, a_class):
