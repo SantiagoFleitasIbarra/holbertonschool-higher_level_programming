661f050378b81a37d06bb3461b86e71d88d5913d
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-10 17:09:46 +0000

    add task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module define a class BaseGeometry"""
+
+
+class BaseGeometry():
+    """This class defines a BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
