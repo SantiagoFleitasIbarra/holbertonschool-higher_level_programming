3f65803bbe701fe11a055efa26d27eea777a7fff
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-10 20:46:01 +0000

    clean

--- python-inheritance/4-inherits_from.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""this module returns True or False"""
-
-
-def inherits_from(obj, a_class):
-    """this defines it"""
-    return issubclass(type(obj), a_class) and type(obj) != a_class
