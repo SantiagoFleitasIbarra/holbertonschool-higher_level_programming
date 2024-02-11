8302a95895690e94a114a483c8512c3ad849a840
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-11 00:04:18 +0000

    add task

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""this module define a function that reads a text file"""
 
 
 def read_file(filename=""):
-    """"""
+    """this defines it"""
     with open(filename, mode='r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
