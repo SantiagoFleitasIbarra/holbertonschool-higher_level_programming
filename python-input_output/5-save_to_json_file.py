316fe328cf464bf2b6096ad454ee11a85fdaea3e
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-11 16:50:08 +0000

    add task

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+this module define a function that writes an Object to a text file,
+using a JSON representation
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """this defines it"""
+    with open(filename, mode='w') as file:
+        json.dump(my_obj, file)
