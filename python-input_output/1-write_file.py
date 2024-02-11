9d1399a5165cfea1acfe6cd2c50a4759c0ec92a3
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-11 01:03:34 +0000

    add task

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""this module define a function that writes a string to a text file"""
 
 
 def write_file(filename="", text=""):
+    """this defines it"""
     with open(filename, mode='w', encoding='utf-8') as file:
         num_chr = file.write(text)
         return num_chr
