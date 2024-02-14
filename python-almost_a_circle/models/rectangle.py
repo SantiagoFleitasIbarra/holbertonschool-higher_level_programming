c051dc632c5cff26ea82e5cfa67e8f91e434f28a
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-14 22:23:42 +0000

    add task

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,7 @@ class Rectangle(Base):
         instance with the character #"""
         for _ in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
+        self.y, self.width, self.height)
