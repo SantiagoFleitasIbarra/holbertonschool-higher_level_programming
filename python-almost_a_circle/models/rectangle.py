7dab5b88479857d95de62cb99c6e558242db1b54
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-14 22:24:47 +0000

    add task

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -74,5 +74,7 @@ class Rectangle(Base):
             print("#" * self.width)
 
     def __str__(self):
+        """method so that it returns
+        [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
         self.y, self.width, self.height)
