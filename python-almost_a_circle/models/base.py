7af59fd05ad4b3eff6a450a5950a5e4dab5d6a8a
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-14 21:26:56 +0000

    add task 0

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,9 +2,11 @@
 """this module define a class Base"""
 
 
-class Base():
+class Base:
     """this define a class constructor"""
+
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
