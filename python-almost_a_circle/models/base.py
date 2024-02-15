fb208777f69af725c69754a19ae524972ac37405
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-15 23:20:16 +0000

    add task

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """this module define a class Base"""
 
 
+import json
+
+
 class Base:
     """this define a class constructor"""
 
@@ -13,3 +16,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
