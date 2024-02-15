4da4d12cd43106268f1c19ed045a7a4bbe01851b
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-15 23:08:19 +0000

    add task

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,12 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
