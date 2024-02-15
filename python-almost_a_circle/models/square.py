acc2d674086fcb57c23cecb44fbb2226141d13a0
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-15 23:11:36 +0000

    add task

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -38,3 +38,11 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
