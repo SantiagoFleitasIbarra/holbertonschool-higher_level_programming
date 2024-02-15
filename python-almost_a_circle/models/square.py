23063a4e2d382d56674494a5f511a9802b02716d
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-15 22:52:08 +0000

    add task

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""""""
+"""this module define a class Square that inherits from Rectangle"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """"""
+    """this define it"""
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
