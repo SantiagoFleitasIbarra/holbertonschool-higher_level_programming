f1d4c30f096b60347758e18ee24260b0c1d3a6d5
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-15 22:52:56 +0000

    add task

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,5 +13,5 @@ class Square(Rectangle):
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width 
+            self.id, self.x, self.y, self.width
         )
