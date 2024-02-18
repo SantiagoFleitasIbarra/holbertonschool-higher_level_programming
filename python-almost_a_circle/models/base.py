c051565655d83ae28044d523c9313d8dbbd368fe
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-18 15:28:02 +0000

    add task

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -40,6 +40,7 @@ class Base:
     def from_json_string(json_string):
         """that returns the list of the JSON
         string representation json_string"""
+
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
