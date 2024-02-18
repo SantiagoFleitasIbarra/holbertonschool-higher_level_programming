2e458b8331da45212d9fd20f2ec4fdd237c76127
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-02-18 14:59:40 +0000

    add task

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,7 +20,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries or len(list_dictionaries) != 0:
-            return json.dumps(list_dictionaries)
-        else:
+        if not list_dictionaries or len(list_dictionaries) == 0:
             return "[]"
+        return json.dumps(list_dictionaries)
