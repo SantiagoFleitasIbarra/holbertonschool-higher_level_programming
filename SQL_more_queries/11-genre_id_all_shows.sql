afa052bc67535f28965096827dcd8521cbdba92e
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-03-17 21:27:55 +0000

    fixed comment

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
--- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+-- Lists all shows contained in the database hbtn_0d_tvshows
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
