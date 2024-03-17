4297b6340d23ff9bfd21dc7428c4398e25b4970c
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-03-17 21:04:22 +0000

    add task

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
